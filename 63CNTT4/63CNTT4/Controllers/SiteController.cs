4c1d520c02c4cb6e21e6314bc1e27b48a60f5185
T-U-NA <tuan.pm.63cntt@ntu.edu.vn>
2023-10-06 18:43:36 +0700

    tao csdl (Entity Framework)

--- 63CNTT4/63CNTT4/Controllers/SiteController.cs
+++ 63CNTT4/63CNTT4/Controllers/SiteController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MyClass.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyClass.Model;
 
 namespace _63CNTT4.Controllers
 {
@@ -11,6 +13,9 @@ namespace _63CNTT4.Controllers
         // GET: Site
         public ActionResult Index()
         {
+            MyDBContext db = new MyDBContext();//tạo mới mẫu tin
+            int somau = db.Products.Count();//ví dụ hiển thị số mẫu tin của Products ra M.hình
+            ViewBag.somau = somau;//truyền dưới dạng ViewBag
             return View();
         }
     }
---
+++ 63CNTT4/MyClass/Model/Categories.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;//Muc dich thiet lap cac truong [KEY],[REQUIRED]
+using System.ComponentModel.DataAnnotations.Schema;//Muc dich: Tap ra bang (table) co ten duoc dinh danh la [Table("Categories"]
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass.Model
+{
+    [Table ("Categories")]   
+    
+    public class Categories
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public String Slug {  get; set; }
+        public int ParentID { get; set; }
+        public int? Order { get; set; }
+        [Required]
+        public string MetaDesc { get; set; }
+        [Required]
+        public string MetaKey { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int CreateBy { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public int UpdateBy { get; set; }
+        public int Status { get; set; }
+    }
+}
